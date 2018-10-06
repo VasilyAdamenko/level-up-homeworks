e165e6bf482917fae6caace70a499f5c6d91071e
VasilyAdamenko <odeen86@gmail.com>
2018-10-06 15:19:00 +0300

    js hw 1

---
+++ public/views/javascript/homeWork-1/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Home work 1. Part 1
+
+var name = 'Василий';
+var admin = name;
+console.log(admin);
+
+// Home work 1. Part 2
+
+var humanPlanet = 'Земля';
+var visitor = 'Я просто спросить';
+
+// Home work 1. Part 3
+
+var fps = 'количество кадров в секунду';
+var hyphen = 'соеденительный знак между словами';
+var viewModeDocument = 'режим просмотра документа';
+var viewLastNavPage = 'необходимость показа последней страницы постраничной навигации';
+
+// в случаях, где уже есть общепринятые обозначения, решил их же и использовать
